148f50a174840d3e829f29633d086cceede53a52
Eric Wang <31226208+wangxpert@users.noreply.github.com>
2025-04-21 21:02:55 +0000

    prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
 type SearchResult = {
-  id: string;
-  title: string;
-  url: string;
-  description: string;
-  category: 'VIDEOS' | 'PLAYLISTS' | 'BLOG_POSTS';
+    id: string;
+    title: string;
+    url: string;
+    description: string;
+    category: "VIDEOS" | "PLAYLISTS" | "BLOG_POSTS";
 };
 
 function App() {
-
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Search UI Challenge</h1>
-                [Your UI here]
+                [This is production]
             </header>
         </div>
     );
